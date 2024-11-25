3dbb8848809e1d589bdf0fbf3b351811816b2f36
YawAsanteBoadu <yawasante95@yahoo.com>
2024-11-25 10:24:56 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Nav from './components/Nav.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Content from './components/Content.jsx';
+
+
+function App () {
+    return (
+        <>
+        <Nav/>
+        <Header/>
+
+        <Content/>
+        
+        <Footer/>
+        
+        
+        </>
+    )
+}
+
+export default App
